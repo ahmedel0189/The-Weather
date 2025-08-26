bd3be312dd87dfba48282883f0bca9b7da89824a
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-08-26 16:51:46 +0300

    Starting to use cubit State management to Changr the state of the home page this is the first action in the pattern 1:Create states

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,17 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:the_weather/Items/Weather%20Search/search_body.dart';
 import 'package:the_weather/Items/custom_appbar.dart';
 import 'package:the_weather/Items/no_weather_now.dart';
+import 'package:the_weather/Items/weather_info_bodt/weather_info_body.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       // backgroundColor: Colors.lightBlue,
-      backgroundColor: Colors.white,
+      // backgroundColor: Colors.white,
       appBar: CustomAppbar(),
-      body: NoWeatherNow(),
+      body: weatherModel == null
+          ? NoWeatherNow()
+          : WeatherInfoBody(),
     );
   }
 }
