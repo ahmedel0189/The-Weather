64e7eb719b17242d73b6a79a73227c24f2432f9d
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-08-27 16:13:01 +0300

    Provide cubit in a way to acsess the target

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:the_weather/Screens/home_page.dart';
+import 'package:the_weather/cubits/get_weather_cubit/cubit_get_weather.dart';
 
 class THEWeather extends StatelessWidget {
   const THEWeather({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title:  "THE Weather",
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
+    return BlocProvider(
+      create: (context) => GetWeatherCubit(),
+      child: MaterialApp(
+        title: "THE Weather",
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+      ),
     );
   }
 }
+
 void main() {
   runApp(const THEWeather());
 }
